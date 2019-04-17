fae1ca15c140f25235a821c87167abbd6c4e8c32
Ian Lovell <ian.lovell96@gmail.com>
2019-04-16 20:50:07 -0500

    Added Dagger dependencies and created the AppComponent class that will hold the inject methods for the application, activity, and fragments

--- app/src/main/java/com/lovell/githubrepoviewer/HomeFragment.kt
+++ app/src/main/java/com/lovell/githubrepoviewer/HomeFragment.kt
@@ -13,5 +13,4 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
-
 }
---
+++ app/src/main/java/com/lovell/githubrepoviewer/di/AppComponent.kt
@@ -0,0 +1,9 @@
+package com.lovell.githubrepoviewer.di
+
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@Component(modules = [])
+interface AppComponent {
+}

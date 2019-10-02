cc8a6e7c258ba52a240043f6b8e7a97b8042359f
Ian Lovell <ian.lovell96@gmail.com>
2019-10-02 08:28:54 -0500

    Moved some more dependencies around and started to add actual code to the network layer

--- network/src/main/java/com/lovell/network/UserService.kt
+++ network/src/main/java/com/lovell/network/UserService.kt
@@ -1,2 +1,10 @@
 package com.lovell.network
 
+import io.reactivex.Single
+import okhttp3.ResponseBody
+import retrofit2.http.GET
+
+interface UserService {
+    @GET("users/{user}/repos")
+    fun getReposForUser(user: String): Single<ResponseBody>
+}
